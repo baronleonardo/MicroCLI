ffb8a6697ce9132ea11ebfdad48665c5222feca6
Baron Leonardo <mohamedayman.fcis@zoho.com>
2019-09-25 00:12:13 +0200

    update `test` command

--- src/microcli/commanddb/commands/test/test.c
+++ src/microcli/commanddb/commands/test/test.c
@@ -2,6 +2,12 @@
 #include "../../../../drivers/drivers.h"
 
 void cmd_test(char args[], uint8_t args_len) {
-    const char out[] = "Test is done";
-    Comm_write(out, sizeof(out) - 1);
+    if(args_len == 0)
+        Comm_write( "Test is done", 
+                    sizeof("Test is done") - 1 );
+    else
+        Comm_write( "This command takes no args!", 
+                    sizeof("This command takes no args!") - 1 );
+
+    Comm_writeNewLine(); 
 }
